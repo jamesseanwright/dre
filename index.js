1c520ece7dccf806322dd7b009b8e8a4bd54ddc6
James Wright <james.wright@yld.io>
2019-05-15 16:55:20 +0100

    Filters and displays unused CSS selectors

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const puppeteer = require('puppeteer');
 const css = require('css');
 const dreConfig = require(`${process.cwd()}/dreconfig.json`);
 
-const getUnusedCSSSelectors = ({ ranges, text }) => {
-  const unusedCSSText = ranges.reduce(
-    (css, range) => css.replace(css.slice(range.start, range.end), ''),
-    text,
-  );
+const getSelectors = ({ stylesheet }) =>
+  stylesheet.rules.flatMap(({ selectors }) => selectors);
 
-  const ast = css.parse(unusedCSSText);
+// TODO: combine separate iterations to aid perf
+const getUnusedSelectors = (ranges, text) => {
+  const allSelectors = getSelectors(css.parse(text));
 
-  return ast.stylesheet.rules
-    .map(rule => rule.selectors)
-    .flat(Number.Infinity);
-}
+  const usedSelectors = ranges
+    .map(({ start, end }) => css.parse(text.slice(start, end)))
+    .flatMap(getSelectors);
+
+  return allSelectors.filter(selector => !usedSelectors.includes(selector));
+};
+
+// TODO: abstract into report builder and test!
+const buildReport = ({ url, ranges, text }) => `
+  Unused selectors in ${url}:
+  ${'-'.repeat(url.length)}
+  ${getUnusedSelectors(ranges, text).join('\n  *')}
+
+  ----
+
+`;
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -23,8 +34,11 @@ const getUnusedCSSSelectors = ({ ranges, text }) => {
 
   await page.coverage.startCSSCoverage();
   await page.goto(`${dreConfig.baseUrl}${dreConfig.paths[0]}`);
-  const reports = await page.coverage.stopCSSCoverage();
-  getUnusedCSSSelectors(reports[4]);
+
+  const coverageResults = await page.coverage.stopCSSCoverage();
+  const reports = coverageResults.map(buildReport);
+
+  console.log(reports.join('\n'));
 
   await browser.close();
 })();
